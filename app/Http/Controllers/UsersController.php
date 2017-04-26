ded2c38e11bd1c5e59f06f8a404b16d3ec83354d
kouno_acw <kouno@app-craftworks.net>
2017-04-26 07:25:33 +0000

    user pages

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::paginate(10);
+        
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+        
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
+
+}
